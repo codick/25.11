bc3f61beca9556adc619c7047d799c61ea279839
codick <114742863+codick@users.noreply.github.com>
2022-11-25 15:36:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// №1
+
+// function rand4() {
+//     return Math.random() * 4;
+//     }
+// function rand8to10(){
+//     return rand4()/2 + 8;
+// }
+
+// №2 
+// function findDuplicates(array, distance){
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] == array[i + distance]) {
+//             return i
+//         }
+//     }
+//     return -1
+// }
+
+// №3
+
+// function hasTerms(array, sum) { 
+//     for(let i = 0; i < array.length; i++){
+//         if(-)
+//     }
+// }
+
+// №4
+
+// function remove(array, x){ 
+//     for(let i = 0; i < array.length; i++){
+//         if(array[i] == x){
+//             for(let j = i; j < array.length; j++){
+//                 array[j] = array[j + 1]; 
+//             }
+//             array.pop();
+//         }
+//     };
+//     return array
+// }
+
+// console.log(remove([1, 2, 3, 2, 3, 4], 2))
+
+
+
+// №5
+
+
+
